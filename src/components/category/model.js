2d3239813af82e033f90e6cb215047e5cffd172a
Dayroot <dallancaceresp@gmail.com>
2021-12-01 21:31:10 -0500

    Agregue un campo de caracteristicas en el modulo de categoria

--- src/components/category/model.js
+++ src/components/category/model.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const mySchema = new Schema({
+const featuresSchema =new Schema({
+    color: {
+        type: String,
+        default: null,
+    },
+    material: {
+        type: String,
+        default: null,
+    },
+    craftType: {
+        type: String,
+        default: null,
+    },
+    department: {
+        type: String,
+        default: null,
+    },
+});
+
+const categorySchema = new Schema({
     name: {
         type: String,
         required: true,
     },
+    features:{
+        type: featuresSchema,
+        default: null,
+    }
+
 });
 
-const model = mongoose.model('Category', mySchema);
+const model = mongoose.model('Category', categorySchema);
 module.exports = model;
